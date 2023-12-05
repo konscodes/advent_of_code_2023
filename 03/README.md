16291cc079e84c19d21dfbc9064bee0c2ce9b601
Kons M <katashi.rain@gmail.com>
2023-12-05 23:43:23 +0900

    Day3 part1 completed

--- 03/README.md
+++ 03/README.md
@@ -19,17 +19,22 @@ Here is an example engine schematic:
 In this schematic, two numbers are not part numbers because they are not adjacent to a symbol: 114 (top right) and 58 (middle right). Every other number is adjacent to a symbol and so is a part number; their sum is 4361.
 
 ### Thoughts
-I think it will be easier to start from symbols. We will go over symbol neighbours and adjesent number and its number neighbours.
+I think it will be easier to start from symbols. We will go over symbol neighbors and adjacent number and its number neighbors.
 
 1. Create a structure map of zeros - grid len(line in data) x len(data.split())
 2. Check the (i,j) of symbols in the data.
-3. Pass this (i,j) as parameters to neighbours function that returns (x,y) of a neighbour that is a number
+3. Pass this (i,j) as parameters to neighbors function that returns (x,y) of a neighbor that is a number
     - Update the map with 1 for (x,y)
-    - Pass (x,y) as parameters to neighbours function
-4. Continue to the next number when neighbours completed running
+    - Pass (x,y) as parameters to neighbors function
+4. Continue to the next number when neighbors completed running
 
 The resulting map will consist of 1s and mark location of numbers we need.
 
 1. For i,j of each element of a map that is 1 fetch i,j and join to number
     - If next i,j is not 1 then append number to a list and set number to ''
 2. Sum int values from the returned list
+
+## Part 2: Gear ratios for all * gears adjacent to two numbers
+The missing part wasn't the only issue - one of the gears in the engine is wrong. A gear is any * symbol that is adjacent to exactly two part numbers. Its gear ratio is the result of multiplying those two numbers together.
+
+In this schematic, there are two gears. The first is in the top left; it has part numbers 467 and 35, so its gear ratio is 16345. The second gear is in the lower right; its gear ratio is 451490. (The * adjacent to 617 is not a gear because it is only adjacent to one part number.) Adding up all of the gear ratios produces 467835.
--- 03/day3.py
+++ 03/day3.py
@@ -2,11 +2,29 @@ import time
 from pathlib import Path
 
 
-def neighbours(position: tuple):
-    # Check the neighbours of position
-    # If numbers update schematic_map and check the neighbours again
+def valid_neighbor(neighbor: tuple):
+    grid_range = range(len(data))
+    x,y = neighbor
+    in_range = bool(x in grid_range and y in grid_range)
+    already_marked = bool(schematic_map[x][y] == 1) if in_range else True
+    return in_range and not already_marked
+
+
+def crawl_neighbors(position: tuple):
+    '''
+    Check the neighbors of position. 
+    If numbers update schematic_map and check the neighbors again
+    '''
     i,j = position
-    return i,j
+    neighbors = [(i-1, j-1), (i-1, j), (i-1, j+1),
+                (i, j-1), (i, j+1),
+                (i+1, j+1), (i+1, j), (i+1, j-1)]
+    for neighbor in neighbors:
+        if valid_neighbor(neighbor):
+            x,y = neighbor
+            if schematic[x][y] in numbers:
+                schematic_map[x][y] = 1
+                crawl_neighbors(neighbor)
 
 
 # Start the timer
@@ -14,32 +32,40 @@ start_time = time.time()
 
 # Set filepath
 script_path = Path(__file__).resolve()
-file_path = script_path.parent / 'test.txt'
+file_path = script_path.parent / 'day3.txt'
 
 with open(file_path) as f:
     # Split the data into list of strings
     data = f.read().splitlines()
 
-filters = '01234567890.'
-
-
-    
 # Schematic with all symbols in it in a grid
-schematic = [[x for x in line] for line in data]
+schematic = [list(line) for line in data]
 
 # Schematic map that will be populated with number locations that we need to sum up
-schematic_map = [[0] * len(data)] * len(data)
+schematic_map = [[0 for _ in range(len(data))] for _ in range(len(data))]
 
-# Schematic map with only special symbol location (i,j)
-symbol_map = [[(i, j) if value not in filters else 0
-               for j, value in enumerate(line)] for i, line in enumerate(data)]
-
+# Take symbol position (i,j) and start crawling neighbors
+numbers = '01234567890'
 for i, line in enumerate(data):
     for j, value in enumerate(line):
-        position = (i, j) if value not in filters
-        test = neighbours(position)
+        if value not in numbers + '.':
+            position = (i, j)
+            crawl_neighbors(position)
 
-print('Part 1:')
+# At this point the schematic map is filled with valid markers
+# Extract corresponding part numbers from schematic
+part_numbers = []
+number = ''
+for i in range(len(schematic_map)):
+    for j in range(len(schematic_map)):
+        if schematic_map[i][j] == 1:
+            number += schematic[i][j]
+        else:
+            part_numbers.append(int(number)) if number else None
+            number = ''
+
+
+print('Part 1:', sum(part_numbers))
 print('Part 2:')
 
 print("--- %s seconds ---" % (time.time() - start_time))
