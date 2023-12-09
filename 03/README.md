22f948b5818b6d46c1b10fc43a6822b913ca2bb7
Kons M <katashi.rain@gmail.com>
2023-12-09 21:47:59 +0900

    Day3 part2 completed

--- 03/README.md
+++ 03/README.md
@@ -18,23 +18,23 @@ Here is an example engine schematic:
 
 In this schematic, two numbers are not part numbers because they are not adjacent to a symbol: 114 (top right) and 58 (middle right). Every other number is adjacent to a symbol and so is a part number; their sum is 4361.
 
+## Part 2: Gear ratios for all * gears adjacent to two numbers
+The missing part wasn't the only issue - one of the gears in the engine is wrong. A gear is any * symbol that is adjacent to exactly two part numbers. Its gear ratio is the result of multiplying those two numbers together.
+
+In this schematic, there are two gears. The first is in the top left; it has part numbers 467 and 35, so its gear ratio is 16345. The second gear is in the lower right; its gear ratio is 451490. (The * adjacent to 617 is not a gear because it is only adjacent to one part number.) Adding up all of the gear ratios produces 467835.
+
 ### Thoughts
 I think it will be easier to start from symbols. We will go over symbol neighbors and adjacent number and its number neighbors.
 
 1. Create a structure map of zeros - grid len(line in data) x len(data.split())
 2. Check the (i,j) of symbols in the data.
 3. Pass this (i,j) as parameters to neighbors function that returns (x,y) of a neighbor that is a number
-    - Update the map with 1 for (x,y)
+    - Update the map with our adjacent symbol coordinates (i,j) This will be required for Part 2
     - Pass (x,y) as parameters to neighbors function
 4. Continue to the next number when neighbors completed running
 
-The resulting map will consist of 1s and mark location of numbers we need.
+The resulting map will consist of tuples() and mark numbers we need.
+We will then construct a dict to match tuples (which are symbol coordinates) to their adjacent numbers. 
+This is probably possible to do during crawling stage since we have access to all variables. However I could not figure out how to do so with the crawling method of choice. 
 
-1. For i,j of each element of a map that is 1 fetch i,j and join to number
-    - If next i,j is not 1 then append number to a list and set number to ''
-2. Sum int values from the returned list
-
-## Part 2: Gear ratios for all * gears adjacent to two numbers
-The missing part wasn't the only issue - one of the gears in the engine is wrong. A gear is any * symbol that is adjacent to exactly two part numbers. Its gear ratio is the result of multiplying those two numbers together.
-
-In this schematic, there are two gears. The first is in the top left; it has part numbers 467 and 35, so its gear ratio is 16345. The second gear is in the lower right; its gear ratio is 451490. (The * adjacent to 617 is not a gear because it is only adjacent to one part number.) Adding up all of the gear ratios produces 467835.
+The second part will just take only those parts that are adjacent to the * symbol. Since we already have a dict matching symbols to all of their adjacent numbers it will be an easy task.
--- 03/day3.py
+++ 03/day3.py
@@ -6,16 +6,16 @@ def valid_neighbor(neighbor: tuple):
     grid_range = range(len(data))
     x,y = neighbor
     in_range = bool(x in grid_range and y in grid_range)
-    already_marked = bool(schematic_map[x][y] == 1) if in_range else True
+    already_marked = bool(schematic_map[x][y]) if in_range else True
     return in_range and not already_marked
 
 
-def crawl_neighbors(position: tuple):
+def crawl_neighbors(starting_coordinates: tuple):
     '''
     Check the neighbors of position. 
     If numbers update schematic_map and check the neighbors again
     '''
-    i,j = position
+    i,j = starting_coordinates
     neighbors = [(i-1, j-1), (i-1, j), (i-1, j+1),
                 (i, j-1), (i, j+1),
                 (i+1, j+1), (i+1, j), (i+1, j-1)]
@@ -23,7 +23,7 @@ def crawl_neighbors(position: tuple):
         if valid_neighbor(neighbor):
             x,y = neighbor
             if schematic[x][y] in numbers:
-                schematic_map[x][y] = 1
+                schematic_map[x][y] = position
                 crawl_neighbors(neighbor)
 
 
@@ -46,26 +46,41 @@ schematic_map = [[0 for _ in range(len(data))] for _ in range(len(data))]
 
 # Take symbol position (i,j) and start crawling neighbors
 numbers = '01234567890'
+gears = []
 for i, line in enumerate(data):
     for j, value in enumerate(line):
         if value not in numbers + '.':
             position = (i, j)
+            if value == '*':
+                gears.append(position)
             crawl_neighbors(position)
 
-# At this point the schematic map is filled with valid markers
-# Extract corresponding part numbers from schematic
-part_numbers = []
+'''At this point the schematic map is filled with valid markers, 
+each one representing coordinates of its adjacent symbol.
+We will extract corresponding part numbers from schematic and 
+populate the dict matching each symbol to its numbers.'''
+part_numbers = {}
 number = ''
+tuple_value = ()
 for i in range(len(schematic_map)):
     for j in range(len(schematic_map)):
-        if schematic_map[i][j] == 1:
+        if schematic_map[i][j]:
+            tuple_value = schematic_map[i][j]
             number += schematic[i][j]
         else:
-            part_numbers.append(int(number)) if number else None
+            part_numbers.setdefault(tuple_value, []).append(int(number)) if number else None
             number = ''
 
+print('Part 1:', sum(sum(numbers) for numbers in part_numbers.values()))
 
-print('Part 1:', sum(part_numbers))
-print('Part 2:')
+'''At this point we have a dict matching symbols to all of their adjacent numbers.
+We just need to take and multiply only those that match Part 2 criteria for a gear.'''
+multiplication_sum = 0
+for key in gears:
+    if key in part_numbers and len(part_numbers[key]) == 2:
+        multiplication_result = part_numbers[key][0] * part_numbers[key][1]
+        multiplication_sum += multiplication_result
+
+print('Part 2:', multiplication_sum)
 
 print("--- %s seconds ---" % (time.time() - start_time))
