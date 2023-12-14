73783b22768e63fa700be39d8cf6f8982aa535d6
Kons M <katashi.rain@gmail.com>
2023-12-14 22:11:12 +0900

    Day 5 part 1 completed

---
+++ 05/day5.py
@@ -0,0 +1,38 @@
+import time
+from pathlib import Path
+
+# Start the timer
+start_time = time.time()
+
+# Set filepath
+script_path = Path(__file__).resolve()
+file_path = script_path.parent / 'day5.txt'
+
+# Prepare the data
+with open(file_path, 'r') as f:
+    data = f.read().split('\n\n')
+    data = {x.split(':')[0]: x.split(':')[1].split('\n') for x in data}
+    seeds = list(data['seeds'][0].split())
+
+def resolve_map(seed, data) -> list:
+    maps = [list(value) for value in data.values()][1:] # all maps excluding seeds
+    pathway = [int(seed)]
+    for mapping in maps:
+        pathway.append(crawl_pathways(mapping, target=pathway[-1]))
+    return pathway
+
+
+def crawl_pathways(mapping, target):
+    target = int(target)
+    for pathway in mapping:
+        if pathway:
+            destination, source, span = [int(x) for x in pathway.split()]
+            if target in range(source, source + span):
+                    new_target = destination - source + target
+                    return new_target
+    return target
+
+
+print('Part 1:', min(resolve_map(seed, data)[-1] for seed in seeds))
+
+print("--- %s seconds ---" % (time.time() - start_time))
