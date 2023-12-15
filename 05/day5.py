296ed724359cd74a6236f60f1a6955b3c16d4592
Kons M <katashi.rain@gmail.com>
2023-12-15 22:05:28 +0900

    Day 5 part 2 brut

--- 05/day5.py
+++ 05/day5.py
@@ -13,6 +13,13 @@ with open(file_path, 'r') as f:
     data = f.read().split('\n\n')
     data = {x.split(':')[0]: x.split(':')[1].split('\n') for x in data}
     seeds = list(data['seeds'][0].split())
+    seed_start = seeds[::2]
+    seed_length = seeds[1::2]
+    seed_ranges = []
+    
+    for index, seed in enumerate(seed_start):
+        seed_range = range(int(seed), int(seed) + int(seed_length[index]))
+        seed_ranges.append(seed_range)
 
 def resolve_map(seed, data) -> list:
     maps = [list(value) for value in data.values()][1:] # all maps excluding seeds
@@ -34,5 +41,8 @@ def crawl_pathways(mapping, target):
 
 
 print('Part 1:', min(resolve_map(seed, data)[-1] for seed in seeds))
+
+lows = [min(resolve_map(seed, data)[-1] for seed in batch) for batch in seed_ranges]
+print('Part 2:', min(lows))
 
 print("--- %s seconds ---" % (time.time() - start_time))
